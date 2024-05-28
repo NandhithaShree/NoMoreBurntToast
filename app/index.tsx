aa79e6cf0fa0d17a02e8d34717a5ee1693887ec6
NandhithaShree <142162325+NandhithaShree@users.noreply.github.com>
2024-05-28 14:16:27 +0800

    placed a logo inside, and putting texts in

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,45 @@
-import { Text, View } from "react-native";
+import { Text, View, Image, StyleSheet, } from "react-native";  
+import * as Font from 'expo-font';
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+    <View style = {styles.container}>
+      <Login/>
+      <DisplayIcon />
     </View>
+    
   );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    paddingTop: 0,
+    backgroundColor: '#fef2f3',
+    flex: 1,
+  },
+  tinyLogo: {
+    width: 300,
+    height: 300,
+  },
+  logintext: {
+    width: 100,
+    justifyContent: 'center',
+  }
+});
+
+const Login = () => {
+  return(
+      <Text style = {styles.logintext}> Please login</Text>
+  )
+}
+
+const DisplayIcon = () => {
+  return(
+    <View style  = {styles.container}>
+      <Image
+        style = {styles.tinyLogo}
+        source={require('C:/Users/nandh/OneDrive/Documents/GitHub/NoMoreBurntToast/assets/images/logoig.png')}
+      />
+    </View>
+  )
 }
